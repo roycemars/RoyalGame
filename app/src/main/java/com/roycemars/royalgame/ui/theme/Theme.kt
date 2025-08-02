f814d734d0615776c7a56a3f826d9ba354537a42
Royce Mars <roycemars@proton.me>
2025-08-02 13:32:38 -0500

    Material theme

--- app/src/main/java/com/roycemars/royalgame/ui/theme/Theme.kt
+++ app/src/main/java/com/roycemars/royalgame/ui/theme/Theme.kt
@@ -9,12 +9,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = RoyalOrange,
+    onPrimary = Color.Black,
+    secondary = RoyalPurple,
+    onSecondary = Color.Black,
+    background = RoyalBlack,
+    onBackground = Color.White,
+    surface = RoyalDark,
+    onSurface = RoyalText,
+    error = Color.Red,
+    onError = Color.White,
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -40,14 +52,15 @@ fun RoyalGameTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = DarkColorScheme
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = RoyalBlack.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
     }
 
     MaterialTheme(
