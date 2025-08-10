13f86e7de7958bd04963f05acf5b8940e53baac5
Royce Mars <roycemars@proton.me>
2025-08-09 21:08:13 -0500

    New icon

--- app/src/main/java/com/roycemars/royalgame/feature/list/ui/ClubListViewModel.kt
+++ app/src/main/java/com/roycemars/royalgame/feature/list/ui/ClubListViewModel.kt
@@ -6,10 +6,6 @@ import com.airbnb.mvrx.MavericksState
 import com.roycemars.royalgame.core.location.LocationRepository
 import com.roycemars.royalgame.core.model.Location
 
-data class MainState(
-    val location: Location
-) : MavericksState
-
 class MainViewModel
 constructor(private val locationRepository: LocationRepository) : ViewModel()
 {
